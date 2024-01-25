99bd296c9b7677706463f8890d5f8b25024a19e4
Pot2709 <jordan.potenciano@ismem.edu.pe>
2024-01-25 12:17:04 -0500

    Agregar archivos de proyecto.

---
+++ ExportarExcel/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ExportarExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnimportar = new System.Windows.Forms.Button();
+            this.tbnexportar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.tbnexportar);
+            this.groupBox1.Controls.Add(this.btnimportar);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(54, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(612, 383);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnimportar
+            // 
+            this.btnimportar.Location = new System.Drawing.Point(202, 37);
+            this.btnimportar.Name = "btnimportar";
+            this.btnimportar.Size = new System.Drawing.Size(75, 36);
+            this.btnimportar.TabIndex = 0;
+            this.btnimportar.Text = "Importar";
+            this.btnimportar.UseVisualStyleBackColor = true;
+            this.btnimportar.Click += new System.EventHandler(this.btnimportar_Click);
+            // 
+            // tbnexportar
+            // 
+            this.tbnexportar.Location = new System.Drawing.Point(327, 37);
+            this.tbnexportar.Name = "tbnexportar";
+            this.tbnexportar.Size = new System.Drawing.Size(87, 36);
+            this.tbnexportar.TabIndex = 1;
+            this.tbnexportar.Text = "Exportar";
+            this.tbnexportar.UseVisualStyleBackColor = true;
+            this.tbnexportar.Click += new System.EventHandler(this.tbnexportar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(600, 279);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 467);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnimportar;
+        private System.Windows.Forms.Button tbnexportar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ ExportarExcel/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel; //Estas son las los llamados a los paquetes nuggets
+using Microsoft.Office.Interop.Excel;  
+using ExcelDataReader;
+using System.IO;
+
+namespace ExportarExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnimportar_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Archivos de Excel|*.xlsx;*.xls|Archivos de Excel 97-2003|*.xls";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    using (var stream = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                        {
+                            var dataSet = reader.AsDataSet(new ExcelDataSetConfiguration()
+                            {
+                                ConfigureDataTable = (tableReader) => new ExcelDataTableConfiguration()
+                                {
+                                    UseHeaderRow = true
+                                }
+                            });
+
+                            dataGridView1.DataSource = dataSet.Tables[0];
+                        }
+                    }
+                    dataGridView1.Columns[0].HeaderText = "Calle";
+                    dataGridView1.Columns[1].HeaderText = "Num";
+                    dataGridView1.Columns[2].HeaderText = "Colonia";
+                    dataGridView1.Columns[3].HeaderText = "Delegacion";
+
+                    dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                    dataGridView1.AutoResizeColumns();
+                    dataGridView1.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11);
+                }
+            }
+
+
+        }
+
+        private void tbnexportar_Click(object sender, EventArgs e)
+        {
+            ExportToExcel(dataGridView1);
+
+        }
+
+        private void ExportToExcel(DataGridView dataGridView)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = true;
+
+            Workbook workbook = excelApp.Workbooks.Add();
+            Worksheet worksheet = (Worksheet)workbook.Sheets[1];
+
+            int columnCount = dataGridView.ColumnCount;
+            for (int i = 0; i < columnCount; i++)
+                for (int j = 0; j < columnCount; j++)
+                {
+                    worksheet.Cells[i + 1, j + 1] = dataGridView.Columns[i].HeaderText;
+                }
+
+
+            int rowCount = dataGridView.RowCount;
+            for (int i = 0; i < rowCount; i++)
+                for (int j = 0; j < columnCount; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = dataGridView.Rows[i].Cells[j].Value;
+                }
+            string tempFile = System.IO.Path.GetTempFileName()+".xls";
+            workbook.SaveAs(tempFile);
+        }
+
+
+
+
+    }
+}
